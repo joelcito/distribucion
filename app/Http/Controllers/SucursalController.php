a1b38ecde44b6a7ea0814fb03c1d09715421d739
janefloresmar <carolineflores75@hotmail.com>
2025-09-11 18:50:45 -0400

    sucursales

---
+++ app/Http/Controllers/SucursalController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Sucursal;
+use App\Http\Requests\StoreSucursalRequest;
+use App\Http\Requests\UpdateSucursalRequest;
+
+class SucursalController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreSucursalRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Sucursal $sucursal)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Sucursal $sucursal)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateSucursalRequest $request, Sucursal $sucursal)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Sucursal $sucursal)
+    {
+        //
+    }
+}
