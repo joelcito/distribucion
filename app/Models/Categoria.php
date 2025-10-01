37ede6bce3ebff92f4f18275e7f7d50875976555
JOEL FLORES QUISPE <joel.flores@segip.gob.bo>
2025-10-01 02:22:27 -0400

    se arghego cambiso

--- app/Models/Categoria.php
+++ app/Models/Categoria.php
@@ -10,7 +10,7 @@ class Categoria extends Model
 {
     use HasFactory, SoftDeletes;
 
-    protected $table = 'roles';
+    protected $table = 'categorias';
 
     protected $fillable =[
         'usuario_creador_id',
